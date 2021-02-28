bcc35a9567dd62d7a6a6a1754318aa12ee1af905
ludotosk <72620499+ludotosk@users.noreply.github.com>
2021-02-28 16:04:23 +0100

    Aggiunti thread

--- server.py
+++ server.py
@@ -1,31 +1,17 @@
+import os
+import socketserver
+import http.server
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
-# Creiamo la classe che riceverà e risponderà alla richieste HTTP
-
-
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-# Implementiamo il metodo che risponde alle richieste GET
-
-    def do_GET(self):
-        # Specifichiamo il codice di risposta
-        self.send_response(200)
-        # Specifichiamo uno o più header
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        # Specifichiamo il messaggio che costituirà il corpo della risposta
-        message = "Hello world!"
-        self.wfile.write(bytes(message, "utf8"))
-        return
-
+dir_esecuzione = os.path.join(os.path.dirname(__file__), 'server')
+os.chdir(dir_esecuzione)
 
 def run():
-    print('Avvio del server...')
-    # Specifichiamo le impostazioni del server
-    # Scegliamo la porta 8081 (per la porta 80 sono necessari i permessi di root)
-    server_address = ('127.0.0.1', 8081)
-    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
-    print('Server in esecuzione...')
-    httpd.serve_forever()
+    PORT = 8000
+
+    handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print("Server started at localhost:" + str(PORT))
+        httpd.serve_forever()
 
 
-run()
